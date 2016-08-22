409df37bf1171367c1c62fcac21997b6e37318f9
Joe Maloney <pkgdemonteam@gmail.com>
2016-08-22 08:32:20 -0400

    Allow using intel driver for legacy chipsets which may require it.

--- overlays/install-overlay/root/TrueOSText.sh
+++ overlays/install-overlay/root/TrueOSText.sh
@@ -12,7 +12,7 @@ i="1"
 do
 
 if [ -e "/usr/local/bin/startx" ] ; then
-	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install (Auto-Detect Video) " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
+	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install (Auto-Detect Video) " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" intel "Start graphical install (Legacy Intel)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 else
   dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 fi
@@ -28,6 +28,9 @@ case $ANS in
            startx 2>/tmp/Xerrors ;;
      scfb) echo "Starting graphical installer (SCFB).. Please wait.."
  cp /root/cardDetect/XF86Config.scfb /etc/X11/xorg.conf
+           startx 2>/tmp/Xerrors ;;
+     intel) echo "Starting graphical installer (Legacy Intel).. Please wait.."
+ cp /root/cardDetect/XF86Config.intel /etc/X11/xorg.conf
            startx 2>/tmp/Xerrors ;;
     utility) /root/TrueOSUtil.sh
               clear ;;
