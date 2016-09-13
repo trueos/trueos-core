c9f188e05ab3ef0204db7fae40279f97c6703b0b
Joe Maloney <pkgdemonteam@gmail.com>
2016-09-13 09:17:32 -0400

    Add amdgpu (radeon) support to installer for testing Changelog: yes

--- overlays/install-overlay/root/TrueOSText.sh
+++ overlays/install-overlay/root/TrueOSText.sh
@@ -12,7 +12,7 @@ i="1"
 do
 
 if [ -e "/usr/local/bin/startx" ] ; then
-	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install (Auto-Detect Video) " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" intel "Start graphical install (Legacy Intel)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
+	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install (Auto-Detect Video) " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" intel "Start graphical install (Legacy Intel)" amd "Start graphical install (Radeon)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 else
   dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 fi
@@ -33,6 +33,9 @@ case $ANS in
      intel) echo "Starting graphical installer (Legacy Intel).. Please wait.."
  cp /root/cardDetect/XF86Config.intel /etc/X11/xorg.conf
            startx 2>/tmp/Xerrors ;;
+     amd) echo "Starting graphical installer (Radeon).. Please wait.."
+ kldload radeonkms
+	   startx 2>/tmp/Xerrors ;;
     utility) /root/TrueOSUtil.sh
               clear ;;
      reboot)  reboot -q ;;
