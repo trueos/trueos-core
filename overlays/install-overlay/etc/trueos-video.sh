900dcaa2b1c5b72c18ae3f0a3396896f69f47f8f
Joe Maloney <pkgdemonteam@gmail.com>
2016-09-28 08:38:10 -0400

    Copy over config for virtualbox and start the guest service if detected

--- overlays/install-overlay/etc/trueos-video.sh
+++ overlays/install-overlay/etc/trueos-video.sh
@@ -43,6 +43,14 @@ fi
 
 loadIntelKMS=no
 
+# First check if we are running as a VirtualBox guest
+pciconf -lv | grep -q "VirtualBox"
+if [ $? -eq 0 ] ; then 
+  cp /root/cardDetect/XF86Config.virtualbox /etc/X11/xorg.conf 
+  service vboxguest onestart 
+  return
+fi
+
 # Check for intel as first card
 echo "$card1" | grep -q -i -e "intel"
 if [ $? -eq 0 ] ; then
