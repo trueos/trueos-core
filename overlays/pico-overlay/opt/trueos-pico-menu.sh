f6aa7f93c66943c0e70cb98b5d0e6f7fa52b8122
Kris Moore <kris@ixsystems.com>
2016-10-09 11:25:54 -0400

    Add initial Pico startup scripts, which will start X in broadcast mode, searching for XDMCP server to connect to

---
+++ overlays/pico-overlay/opt/trueos-pico-menu.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ -e /tmp/.trueos-pico-session ] ; then
+  exit 0
+fi
+
+# Only need this script run once a session
+touch /tmp/.trueos-pico-session
+
+# Clear the screen
+clear
+
+PATH="$PATH:/usr/local/bin:/usr/local/sbin"
+export PATH
+
+# Platform specific bits
+dmesg | grep -q "RPI2"
+if [ $? -eq 0 ] ; then
+  . /opt/RPI2/setup
+fi
+
+echo "Starting X in 5 seconds, press ENTER for menu"
+read -t 5 tmp
+if [ $? -eq 0 ] ; then
+  echo "Pressed ENTER"
+else
+  /opt/startx
+fi
+
+exit 0
