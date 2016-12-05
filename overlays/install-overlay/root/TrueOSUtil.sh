8fe788a6a516a294132aeea6041368277ef7127d
Jeff 'Raid' Baitis <jeff@baitis.net>
2016-12-05 09:33:29 -0800

    Even better: refer to storage pools as "storage pools" where space is permitting
    Consistency with other places in the UI

--- overlays/install-overlay/root/TrueOSUtil.sh
+++ overlays/install-overlay/root/TrueOSUtil.sh
@@ -15,7 +15,7 @@ i="1"
 do
 
 # Display Utility Menu
-dialog --title "TrueOS Utility Menu" --menu "Please select from the following options:" 20 55 15 shell "Drop to emergency shell" zimport "Import / mount pool" fixgrub "Restamp GRUB on disk" exit "Exit Utilities" 2>/tmp/UtilAnswer
+dialog --title "TrueOS Utility Menu" --menu "Please select from the following options:" 20 55 15 shell "Drop to emergency shell" zimport "Import / mount storage pool" fixgrub "Restamp GRUB on disk" exit "Exit Utilities" 2>/tmp/UtilAnswer
 
 ANS="`cat /tmp/UtilAnswer`"
 
