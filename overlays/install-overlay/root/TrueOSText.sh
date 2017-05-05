b1365c72f58c9816edc09cd6fd431f5ce8ff8e33
Joe Maloney <jmaloney@joe-desktop.tn.ixsystems.com>
2017-05-05 10:28:25 -0400

    Remove auto-detect wording since we will only use VESA

--- overlays/install-overlay/root/TrueOSText.sh
+++ overlays/install-overlay/root/TrueOSText.sh
@@ -12,7 +12,7 @@ i="1"
 do
 
 if [ -e "/usr/local/bin/startx" ] ; then
-	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install (Auto-Detect Video) " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" intel "Start graphical install (Legacy Intel)" amd "Start graphical install (Radeon)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
+	dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 xorg "Start graphical install " vesa "Start graphical install (VESA)" scfb "Start graphical install (SCFB)" intel "Start graphical install (Legacy Intel)" amd "Start graphical install (Radeon)" install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 else
   dialog --title "TrueOS Installation Menu" --menu "Please select from the following options:" 20 55 15 install "Start text install" utility "System Utilities" reboot "Reboot the system" 2>/tmp/answer
 fi
@@ -21,7 +21,7 @@ ANS="`cat /tmp/answer`"
 
 case $ANS in
     install) /usr/local/bin/pc-installdialog ;;
-    xorg) echo "Starting graphical Installer (AutoDetect).. Please wait.."
+    xorg) echo "Starting graphical Installer .. Please wait.."
           start_xorg ;;
      vesa) echo "Starting graphical Installer (VESA).. Please wait.."
  cp /root/cardDetect/XF86Config.compat /etc/X11/xorg.conf
