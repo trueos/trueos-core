f424a17dbef415b8fb46b45a2269687b43f564fa
Joe Maloney <jmaloney@joes-imac.attlocal.net>
2017-05-28 15:27:43 -0400

    Always use vboxvideo for VirtualBox.  VESA crashes on OS X host.

--- overlays/install-overlay/etc/trueos-video.sh
+++ overlays/install-overlay/etc/trueos-video.sh
@@ -2,4 +2,16 @@
 # Load the vesa driver for the installer 
 ###################################################################
 
-cp /root/cardDetect/XF86Config.compat /etc/X11/xorg.conf
+# First check if we are running as a VirtualBox guest
+pciconf -lv | grep -q "VirtualBox"
+if [ $? -eq 0 ] ; then 
+  cp /root/cardDetect/XF86Config.virtualbox /etc/X11/xorg.conf
+  kldload vboxguest 
+  return
+fi
+
+if [ `sysctl -n machdep.bootmethod` = "BIOS" ] ; then
+  cp /root/cardDetect/XF86Config.compat /etc/X11/xorg.conf
+else
+  cp /root/cardDetect/XF86Config.scfb /etc/X11/xorg.conf
+fi
