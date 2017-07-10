7816dbcc3d46693f84537a786fef133e28df9f30
Kris Moore <kris@ixsystems.com>
2017-07-09 20:22:08 -0400

    Set the default package set on installation

--- xtrafiles/local/share/trueos/scripts/sys-init.sh
+++ xtrafiles/local/share/trueos/scripts/sys-init.sh
@@ -70,6 +70,12 @@ if [ $? -ne 0 ] ; then
   echo "vfs.zfs.arc_max=\"${zArc}M\"" >> /boot/loader.conf
 fi
 
+# Set the default pkg branch
+if [ -e "/root/defaultpkgbranch" ] ; then
+  DPB=`cat /root/defaultpkgbranch`
+  sed -i '' "/PACKAGE_SET:/s/.*/PACKAGE_SET: $DPB/" /usr/local/etc/trueos.conf
+fi
+
 ################################################
 # Do desktop specific init
 ################################################
