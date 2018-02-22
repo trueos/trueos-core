b6c776a6670bacdd6d4b9ac7f23355157c526ab2
Ken Moore <ken@ixsystems.com>
2018-02-22 11:57:00 -0500

    Cleanup how the XDG environment variables are set. Move them out of .xprofile to the 01_xdg-user-dirs script (for update-ability later)

--- package-files/desktop/usr/local/share/trueos/xstartup/01_xdg-user-dirs.sh
+++ package-files/desktop/usr/local/share/trueos/xstartup/01_xdg-user-dirs.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-#Simple script to check for the standardized user directories and create them as needed
+#Setup XDG environment variables (may be modified by DE/WM later)
+export XDG_DATA_DIRS="/share:/usr/share:/usr/local/share"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CONFIG_DIRS="/usr/local/etc/xdg"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_RUNTIME_DIR="/tmp/.runtime-${USERNAME}"
+
+#Check the standardized user directories and create them as needed
 # NOTE: This tool will export a bunch of XDG_* environment variables as well
 if [ -x /usr/local/bin/xdg-user-dirs-update ] ; then
   xdg-user-dirs-update
