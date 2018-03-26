2b49d6b15df093973f50e76ae67cae2b797ff3e6
Ken Moore <ken@ixsystems.com>
2018-03-26 12:40:39 -0400

    Update 02_fix-gtk-environ.sh
    Use floating-point accuracy for the GDK_DPI_SCALE environment variable (2 decimel places).
    This makes the GTK applications scale much better according to the DPI of the screen.

--- package-files/desktop/usr/local/share/trueos/xstartup/02_fix-gtk-environ.sh
+++ package-files/desktop/usr/local/share/trueos/xstartup/02_fix-gtk-environ.sh
@@ -7,8 +7,8 @@ export MOZ_DISABLE_IMAGE_OPTIMIZE=1
 # Detect the current DPI and try to automatically set the GTK scale factor
 if [ -x /usr/local/bin/xdpyinfo ] ; then
   DPI=`xdpyinfo | grep resolution | cut -w -f 3 | cut -d x -f 1`
-  #Text scaling (this would be better with floating-point accuracy in the future)
-  export GDK_DPI_SCALE=`expr ${DPI} / 96`
+  #Text scaling (2 decimel places)
+  export GDK_DPI_SCALE=`bc -e "scale = 2 ; ${DPI} / 96" -e quit`
   #All Scaling (whole numbers only)
   #export GDK_SCALE=
 fi
